9d38e45021ed40eccf74f1e1272c7b0e5ccaf3a3
Yevhen <buhaienkoyevhen@gmail.com>
2020-10-26 14:45:11 +0200

    Добавлено первое окно и меню с диалоговыми окнами

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    String msg ="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +18,104 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    //+++
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_setting, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_location: {
+                msg = "action_locations";
+                AlertDialog aboutDialog = new AlertDialog.Builder(MainActivity.this).create();
+                aboutDialog.setTitle("Выбрать локацию");
+                aboutDialog.setMessage("Выбираем населенный пункт");
+                aboutDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                aboutDialog.show();
+            }
+            break;
+            case R.id.action_format_Temperaturi: {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+                alertDialog.setTitle("Формат температуры");
+                String[] items = {"Фаренгейт","Цельсий"};
+                int checkedItem = 1;
+                alertDialog.setSingleChoiceItems(items, checkedItem, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+//                                Toast.makeText(MainActivity.this, "Фаренгейт", Toast.LENGTH_LONG).show();
+                                msg ="Фаренгейт";
+
+                                break;
+                            case 1:
+                                //                               Toast.makeText(MainActivity.this, "Цельсий", Toast.LENGTH_LONG).show();
+                                msg ="Цельсий";
+
+                                break;
+
+                        }
+                        dialog.dismiss();
+                    }
+
+                });
+                AlertDialog alert = alertDialog.create();
+                alert.setCanceledOnTouchOutside(false);
+                alert.show();
+            }
+            break;
+            case R.id.action_theme_dark: {
+                msg = "theme_dark";
+                AlertDialog aboutDialog = new AlertDialog.Builder(MainActivity.this).create();
+                aboutDialog.setTitle("Выбор темной темы");
+                aboutDialog.setMessage("Пустрь будет темная тема");
+                aboutDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                aboutDialog.show();
+
+            }
+            break;
+            case R.id.action_author: {
+                msg = "author";
+                AlertDialog aboutDialog = new AlertDialog.Builder(MainActivity.this).create();
+                aboutDialog.setTitle("Разработчик программы");
+                aboutDialog.setMessage("(c)2020 студент академии ШАГ\nБугаенко Евгений.");
+                aboutDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        }
+                );
+                aboutDialog.show();
+            }
+            break;
+            default:
+
+        }
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+        return super.onOptionsItemSelected(item);
+
+    }
 }
